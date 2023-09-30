2ca0bc4f152e636c60e989229f9feff48780b13a
Javier Carranza <javcarr2203@gmail.com>
2023-09-30 15:31:35 -0600

    Tarea 2

---
+++ Parcial 2/ModuloCommon/calcularSuma.js
@@ -0,0 +1,5 @@
+let operacion = require("./suma.js");
+
+let a = operacion.SumarNumeros(4, 5);
+
+console.log("La suma es: " + a);
---
+++ Parcial 2/ModuloCommon/suma.js
@@ -0,0 +1,12 @@
+function SumarNumeros(num1, num2){
+    return (num1 + num2);
+}
+
+// module.exports.CalcularAreaCuadrado = CalcularAreaCuadrado;
+// module.exports.CalcularAreaTriangulo = CalcularAreaTriangulo
+
+module.exports.SumarNumeros = SumarNumeros;
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(module);
