414f6fa8dfbc7253e4e3c0ad3079fddb99c97a6b
Javier Carranza <javcarr2203@gmail.com>
2023-10-03 18:01:43 -0600

    Tarea 3

---
+++ Parcial 2/ModuloEcma/calcularSuma.js
@@ -0,0 +1,5 @@
+import { SumarNumeros } from "./suma.js";
+
+let a = SumarNumeros(4, 5);
+
+console.log("La suma es: " + a);
---
+++ Parcial 2/ModuloEcma/suma.js
@@ -0,0 +1,7 @@
+export function SumarNumeros(num1, num2){
+    return (num1 + num2);
+}
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(module);
